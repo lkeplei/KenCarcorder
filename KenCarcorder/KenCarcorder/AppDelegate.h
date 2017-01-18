91d6a6b3031cd7cf48f5fdc962636d4d3c11f9f9
Ken.Liu <liukun@aixuedai.com>
2017-01-18 10:25:33 +0800

    工程创建

---
+++ KenCarcorder/KenCarcorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KenCarcorder
+//
+//  Created by Ken.Liu on 2017/1/18.
+//  Copyright © 2017年 Ken.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KenCarcorder/KenCarcorder/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KenCarcorder
+//
+//  Created by Ken.Liu on 2017/1/18.
+//  Copyright © 2017年 Ken.Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
