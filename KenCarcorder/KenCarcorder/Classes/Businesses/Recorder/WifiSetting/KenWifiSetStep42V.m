f9edda3a0490d0c2d0b21b5d51ccef4a036a2573
Ken.Liu <liukun@aixuedai.com>
2017-02-25 19:02:16 +0800

    连接第二步

--- KenCarcorder/KenCarcorder/Classes/Businesses/Recorder/WifiSetting/KenWifiSetStep42V.m
+++ KenCarcorder/KenCarcorder/Classes/Businesses/Recorder/WifiSetting/KenWifiSetStep42V.m
@@ -33,7 +33,35 @@
 
 #pragma mark - private method
 - (void)initView {
+    UILabel *label1 = [UILabel labelWithTxt:@"请将手机靠近行车记录仪" frame:CGRectZero
+                                       font:[UIFont appFontSize17] color:[UIColor appWhiteTextColor]];
+    [self addSubview:label1];
+    
+    UIImageView *camera = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"wifi_4_camera"]];
+    [self addSubview:camera];
+    
+    UIButton *button = [UIButton buttonWithImg:@"下一步" zoomIn:NO image:[UIImage imageNamed:@"wifi_1_btn"]
+                                      imagesec:nil target:_parentVC action:@selector(nextStep)];
+    button.titleLabel.font = [UIFont appFontSize15];
+    [self addSubview:button];
+    
+    //autolayout
+    [label1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top).offset(kKenOffsetY(213));
+        make.height.mas_equalTo(20);
+        make.centerX.equalTo(self.mas_centerX);
+        make.width.equalTo(self.mas_width);
+    }];
 
+    [camera mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(label1.mas_bottom).offset(kKenOffsetY(110));
+        make.centerX.equalTo(self.mas_centerX);
+    }];
+
+    [button mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(camera.mas_bottom).offset(kKenOffsetY(60));
+        make.centerX.equalTo(self.mas_centerX);
+    }];
 }
 
 @end
