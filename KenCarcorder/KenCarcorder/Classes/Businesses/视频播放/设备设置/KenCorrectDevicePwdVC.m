8cdec03f46bb816cadadb27e6bc84590ab751a7c
刘坤 <liukun927nh@163.com>
2017-05-06 13:10:12 +0800

    更正设备 密码完成 ，至此设备设置功能全部结束，下面来做回放

--- KenCarcorder/KenCarcorder/Classes/Businesses/视频播放/设备设置/KenCorrectDevicePwdVC.m
+++ KenCarcorder/KenCarcorder/Classes/Businesses/视频播放/设备设置/KenCorrectDevicePwdVC.m
@@ -9,9 +9,10 @@
 #import "KenCorrectDevicePwdVC.h"
 #import "KenDeviceDM.h"
 
-@interface KenCorrectDevicePwdVC ()
+@interface KenCorrectDevicePwdVC ()<UIAlertViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) KenDeviceDM *deviceInfo;
+@property (nonatomic, strong) UITextField *pwdTextField;
 
 @end
 
@@ -21,7 +22,7 @@
 - (instancetype)initWithDevice:(KenDeviceDM *)device {
     self = [super init];
     if (self) {
-        
+        _deviceInfo = device;
     }
     return self;
 }
@@ -30,6 +31,102 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self setNavTitle:@"更正密码"];
+    
+    _pwdTextField = [self addTextFiled:@"请输入更正密码" offY:100];
+    
+    UIButton *showBtn = [UIButton buttonWithImg:@"显示密码" zoomIn:NO image:nil imagesec:nil target:self
+                                         action:@selector(showBtnClicked)];
+    [showBtn setBackgroundColor:[UIColor colorWithHexString:@"#419FFF"]];
+    showBtn.layer.cornerRadius = 6;
+    showBtn.frame = CGRectMake(MainScreenWidth * 0.05, CGRectGetMaxY(_pwdTextField.frame) + 35, MainScreenWidth  * 0.9, 40);
+    [self.view addSubview:showBtn];
+    
+    UIButton *confirmBtn = [UIButton buttonWithImg:@"确认修改" zoomIn:NO image:nil imagesec:nil target:self
+                                            action:@selector(confirmBtnClicked)];
+    [confirmBtn setBackgroundColor:[UIColor colorWithHexString:@"#419FFF"]];
+    confirmBtn.layer.cornerRadius = 6;
+    confirmBtn.frame = (CGRect){showBtn.originX, CGRectGetMaxY(showBtn.frame) + 20, showBtn.size};
+    [self.view addSubview:confirmBtn];
+    
+    //tap gesture
+    UITapGestureRecognizer *tapTouch = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard)];
+    tapTouch.delegate = self;
+    [self.view addGestureRecognizer:tapTouch];
+}
+
+- (UITextField *)addTextFiled:(NSString *)content offY:(CGFloat)offY {
+    UITextField *textField = [[UITextField alloc]initWithFrame:CGRectMake(MainScreenWidth * 0.05, offY, MainScreenWidth * 0.9, 40)];
+    textField.placeholder = content;
+    textField.font = [UIFont appFontSize14];
+    textField.clearButtonMode = UITextFieldViewModeAlways;
+    textField.secureTextEntry = NO;
+    textField.clearsOnBeginEditing = NO;
+    textField.textAlignment = NSTextAlignmentLeft;
+    textField.delegate = self;
+    [textField setBackgroundColor:[UIColor whiteColor]];
+    
+    textField.leftView = [[UIView alloc] initWithFrame:(CGRect){0, 0, 15, textField.height}];
+    textField.leftViewMode = UITextFieldViewModeAlways;
+    
+    textField.layer.cornerRadius = 6;
+    textField.layer.masksToBounds = YES;
+    textField.layer.borderWidth = 1;
+    textField.layer.borderColor = [[UIColor appSepLineColor] CGColor];
+    
+    [self.view addSubview:textField];
+    
+    return textField;
+}
+
+#pragma mark - button
+- (void)confirmBtnClicked {
+    if ([_pwdTextField.text length] > 0) {
+        [[KenUserInfoDM getInstance] saveDevicePwd:[_pwdTextField text] device:_deviceInfo];
+        [self.navigationController popViewControllerAnimated:YES];
+    } else {
+        [self showToastWithMsg:@"请输入更正密码"];
+    }
+}
+
+- (void)showBtnClicked {
+    UIAlertView *dialog = [[UIAlertView alloc] initWithTitle:@"请输入登录密码" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定",nil];
+    [dialog setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    [dialog show];
+}
+
+#pragma mark - alert delegate
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        UITextField *pwdTextField = [alertView textFieldAtIndex:0];
+        NSString *pwd = [pwdTextField text];
+        if ([pwd isEqualToString:[KenUserInfoDM getInstance].userPwd]) {
+            [_pwdTextField setText:_deviceInfo.pwd];
+        } else {
+            [self showToastWithMsg:@"密码输入错误"];
+        }
+    }
+}
+
+#pragma mark - textField
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+}
+
+- (void)hideKeyboard {
+    [_pwdTextField resignFirstResponder];
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    if ([touch.view isKindOfClass:[UIControl class]] ||
+        [NSStringFromClass([touch.view class]) isEqualToString:@"UITableViewCellContentView"]) {
+        return NO;
+    }
+    return YES;
 }
 
 @end
